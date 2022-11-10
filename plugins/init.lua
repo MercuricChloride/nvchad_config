8bd46c5ce0c31d70f21f6b3209e1e3e81ca92709
mercuricchloride <mercuryprotocol@protonmail.com>
2022-11-10 12:31:20 -0700

    nvchad custom folder

---
+++ plugins/init.lua
@@ -0,0 +1,42 @@
+-- custom/plugins/init.lua
+-- we're basically returning a table!
+return {
+
+  -- Install plugin
+  ["Pocco81/TrueZen.nvim"] = {},
+
+  -- Override plugin definition options
+  ["goolord/alpha-nvim"] = {
+    disable = false,
+    cmd = "Alpha",
+  },
+
+  -- Override plugin config
+  ["williamboman/mason.nvim"] = {
+    override_options = {
+        ensure_installed = {
+        "html-lsp",
+        "clangd",
+        "solc",
+        "tsserver"
+        }
+      }
+  },
+
+   -- Override plugin config if it has a module called
+   -- If you wish to call a module, which is 'cmp' in this case
+   ["hrsh7th/nvim-cmp"] = {
+    override_options = function()
+      local cmp = require "cmp"
+
+      return {
+        mapping = {
+          ["<C-d>"] = cmp.mapping.scroll_docs(-8),
+        },
+      }
+    end,
+  },
+
+  -- remove plugin
+  -- ["neovim/nvim-lspconfig"] = false
+}
---
+++ plugins/null-ls.lua
@@ -0,0 +1,25 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+   return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  -- b.formatting.deno_fmt,
+  b.formatting.prettier,
+
+  -- solidity stuff
+  b.formatting.solhint,
+
+  -- Lua
+  b.formatting.stylua,
+}
+
+null_ls.setup {
+   debug = true,
+   sources = sources,
+}
