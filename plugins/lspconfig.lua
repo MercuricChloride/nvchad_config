de4b5c794da743239567f5c0643fe9b4711b11eb
mercuricchloride <mercuryprotocol@protonmail.com>
2022-11-21 08:16:44 -0700

    lsp updates

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -2,7 +2,23 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "solc", "tsserver", "rls"}
+local servers = { "html", "cssls", "clangd", "tsserver", "rls"}
+
+lspconfig.solidity.setup({
+  on_attach = on_attach, -- probably you will need this.
+  capabilities = capabilities,
+  settings = {
+    -- example of global remapping
+    solidity = {
+        includePath = '',
+        remapping = {
+          ["@OpenZeppelin/"] = 'OpenZeppelin/openzeppelin-contracts@4.6.0/',
+          ["forge-std/"] = 'lib/forge-std/src/',
+          ["ds-test/"] = 'lib/forge-std/lib/ds-test/src/',
+        }
+    }
+  },
+})
 
 for _, lsp in ipairs(servers) do
 
